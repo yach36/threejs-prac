041a02766d2be172c74b044a2192ae82d6cbd5c2
yasuda.chuma <86759515+yach36@users.noreply.github.com>
2023-10-22 11:02:59 +0900

    add scene and camera

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { WebGLRenderer } from "three";
+import { PerspectiveCamera, Scene, WebGLRenderer } from "three";
 
 const renderer = new WebGLRenderer({
   canvas: document.querySelector("#myCanvas")!,
@@ -7,3 +7,8 @@ const width = 960;
 const height = 540;
 renderer.setSize(width, height);
 renderer.setPixelRatio(window.devicePixelRatio);
+
+const scene = new Scene();
+
+const camera = new PerspectiveCamera(45, width / height, 1, 10000);
+camera.position.set(0, 0, 1000);
