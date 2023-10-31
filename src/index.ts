6feeea2d63a2ceed3a88dbbd48f5c98178b26a4b
yasuda.chuma <86759515+yach36@users.noreply.github.com>
2023-11-01 02:25:51 +0900

    update script

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,38 @@
-import { PerspectiveCamera, Scene, WebGLRenderer } from "three";
+import { BoxGeometry, DirectionalLight, Mesh, MeshStandardMaterial, PerspectiveCamera, Scene, WebGLRenderer } from "three";
 
-const renderer = new WebGLRenderer({
-  canvas: document.querySelector("#myCanvas")!,
-});
+const init = () => {
+  // レンダラーの設定
+  const renderer = new WebGLRenderer({
+    canvas: document.querySelector("#canvas")!,
+  });
+  const width = 960;
+  const height = 540;
+  renderer.setSize(width, height);
+  renderer.setPixelRatio(window.devicePixelRatio); // デバイスピクセル比を設定
 
-// レンダラーのサイズを指定
-const width = 960;
-const height = 540;
-renderer.setSize(width, height);
+  // カメラの設定
+  const camera = new PerspectiveCamera(45, width / height, 1, 10000); // new PerspectiveCamera(画角, アスペクト比, 描画開始距離, 描画終了距離)
+  camera.position.set(0, 0, 1000); // カメラの初期座標を設定(x, y, z)
 
-// デバイスピクセル比を設定
-renderer.setPixelRatio(window.devicePixelRatio);
+  // シーンの作成
+  const scene = new Scene();
 
-const scene = new Scene();
+  // メッシュの作成 geometry + material = mesh
+  const geometry = new BoxGeometry(500, 500, 500);
+  const material = new MeshStandardMaterial({
+    color: 0x0000ff,
+  });
+  const box = new Mesh(geometry, material);
+  scene.add(box);
 
-// new PerspectiveCamera(画角, アスペクト比, 描画開始距離, 描画終了距離)
-const camera = new PerspectiveCamera(45, width / height, 1, 10000);
+  // ライトの設定
+  const light = new DirectionalLight(0xffffff);
+  light.intensity = 2; // 光の強さを倍にする
+  light.position.set(1, 1, 1); // ライトの方向
+  scene.add(light);
 
-// カメラの初期座標を設定(x, y, z)
-camera.position.set(0, 0, 1000);
+  // レンダリング
+  renderer.render(scene, camera);
+};
+
+window.addEventListener("DOMContentLoaded", init);
